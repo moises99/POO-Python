c2f38f253773cc43a84c0736ee91cfd9b6a28c9c
moises99 <moisesp.silva@outlook.com.br>
2026-03-07 00:36:23 -0300

    :arrow_up: Ex04

--- Atividades/ex04.py
+++ Atividades/ex04.py
@@ -4,32 +4,25 @@ class Livro:
     def __init__(self,titulo,total_paginas):
         self.titulo = titulo
         self.paginas = total_paginas
-    def avancar_pagina(self,avancar_pg):
         self.pg = 1
-        self.avancar_pg = avancar_pg + 1
-
-        while self.pg < self.avancar_pg:
+    def avancar_pagina(self,avancar_pg):
+          for c in range(avancar_pg):
             if self.pg >= self.paginas:
-              print(f' voce avançou {self.paginas-1} paginas e chegou ao FIM DO LIVRO')
+              print(f'Parabéns, voce terminou o livro [bold red]{self.titulo}[/].')
               break
-            else:
-              self.pg +=1
-              print(f'Página {self.pg} -> ',end="")
-              sl(.2)
-              if  self.pg == self.avancar_pg: 
-                print(f' Você avançou {self.avancar_pg-1} páginas e está na página {self.pg}.')
+            self.pg +=1
+            print(f'Página {self.pg} -> ',end="")
+            sl(.2)
+          if avancar_pg <= self.paginas:
+            print(f' Você avançou {avancar_pg} páginas e está na página {self.pg}.')
+
     
-
-
-
 livro = Livro('Aprendendo POO',20)
+
 print(f'Você acabou de abrir o livro [bold red]{livro.titulo}[/] que tem [bold red]{livro.paginas} páginas[/], agora você está na pagina 1.')
+
 livro.avancar_pagina(5)
-
-
-
-
-
-
-
+livro.avancar_pagina(3)
+livro.avancar_pagina(10)
+livro.avancar_pagina(100)
 
